78e7f3e16c12a34221d54290be01576a29c2632f
jdytrych@m2000.cz <jdytrych@m2000.cz@DESKTOP-D6VBC2I>
2026-08-21 08:01:08 +0200

    20260821800 Signed-off-by: jdytrych@m2000.cz <jdytrych@m2000.cz@DESKTOP-D6VBC2I>

--- src/main/java/cz/burios/qpx/darwin/controller/TestController.java
+++ src/main/java/cz/burios/qpx/darwin/controller/TestController.java
@@ -1,7 +1,14 @@
 package cz.burios.qpx.darwin.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -27,5 +34,32 @@ public class TestController {
 			e.printStackTrace();
 		}
 		return view;
-	}	
+	}
+	
+	@GetMapping("/test/{name}")
+	public ModelAndView page(@PathVariable("name") String name) {
+		/*
+		"button";
+		"buttongroup";
+		"dropdownbutton";
+		"layout";
+		// "photo";
+		"template";
+		"toolbar";
+		*/
+		String path = "test/" + name;
+		System.out.println("TestController.page.path: " + path);
+		ModelAndView view = new ModelAndView(path);
+		try {
+			java.util.Date now = new java.util.Date();
+			LocalDateTime ldtNow = LocalDateTime.now();
+			String timeNo = ldtNow.format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmmssSSS"));
+			System.out.println("timeNo: " + timeNo);
+			view.addObject("timeNo", DateFormatUtils.format(now, "yyyyMMdd.HHmmssSSS"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return view;
+	}
+
 }
